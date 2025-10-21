48189d3eda984a112cb9193f51472055b7b5c488
Jayaharsha Divi <jayaharsha.divi@defence.gov.au>
2025-10-21 18:44:16 +1100

    memory and context

--- src/main/java/tsmr/booking/ai/BookingAssistant.java
+++ src/main/java/tsmr/booking/ai/BookingAssistant.java
@@ -1,5 +1,7 @@
 package tsmr.booking.ai;
 
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.service.AiServices;
 import org.springframework.stereotype.Service;
@@ -15,9 +17,12 @@ public class BookingAssistant {
                 .modelName("gpt-3.5-turbo")
                 .build();
 
+        ChatMemory memory = MessageWindowChatMemory.withMaxMessages(20);
+
         this.chatAgent = AiServices.builder(ChatAgent.class)
                 .chatLanguageModel(model)
                 .tools(tools)
+                .chatMemory(memory)
                 .build();
     }
 
--- src/main/java/tsmr/booking/ai/ChatAgent.java
+++ src/main/java/tsmr/booking/ai/ChatAgent.java
@@ -4,6 +4,6 @@ import dev.langchain4j.service.SystemMessage;
 
 public interface ChatAgent {
 
-    @SystemMessage("You are a helpful AI assistant that helps users query flights and bookings.")
+    @SystemMessage("You are a helpful AI assistant that helps users query flights and bookings. Your job is to help users query and book flights. When providing users with queried results, make sure to convert dates to human readable date time format.")
     String chat(String userMessage);
 }
