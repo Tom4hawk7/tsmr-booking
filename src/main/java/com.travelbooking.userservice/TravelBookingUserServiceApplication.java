3dcf6047eb34f9ccceb9853ffe74a82e66a82d45
Matthew Steele <matthewsteele05@gmail.com>
2025-10-21 12:30:14 +1100

    Add user auth microservice with JWT and Kafka integration

---
+++ src/main/java/com.travelbooking.userservice/TravelBookingUserServiceApplication.java
@@ -0,0 +1,11 @@
+package com.travelbooking.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBookingUserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TravelBookingUserServiceApplication.class, args);
+    }
+}
